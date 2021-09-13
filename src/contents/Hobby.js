f07d76eb3a7812fdce9dd048a1fa2abafd0d451b
bbibbu0223 <80902808+bbibbu0223@users.noreply.github.com>
2021-09-13 09:58:12 +0900

    Add files via upload

---
+++ src/contents/Hobby.js
@@ -0,0 +1,7 @@
+// export default function Hobby(){
+//     return(
+//     <div>
+//         취미자리
+//     </div>
+//     );
+// }
---
+++ src/contents/Main.js
@@ -0,0 +1,14 @@
+export default function Main(){
+    
+
+    return(
+        
+        <div className='container'>
+            
+            <p className="main-txt">Welcome!</p>
+            <video autoplay loop muted controls>
+                <source src="img/Record - 65390.mp4" type="video/mp4"></source>
+            </video>
+        </div>
+    );
+}
